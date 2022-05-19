4e85288d6ee6d7c03d3520f6df1aef25a0d4e7ac
Icaro Davi Tavares Monteiro <icarodavi@gmail.com>
2022-05-19 16:59:16 -0300

    Auth Mock Service

--- lib/core/models/chat_user.dart
+++ lib/core/models/chat_user.dart
@@ -1,12 +1,14 @@
 class ChatUser {
   final String id;
   final String name;
+  final String password;
   final String email;
   final String imageURL;
 
   const ChatUser({
     required this.id,
     required this.name,
+    required this.password,
     required this.email,
     required this.imageURL,
   });
--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -1,23 +1,40 @@
+import 'dart:math';
+
 import 'package:chat/core/models/chat_user.dart';
 import 'dart:io';
+import 'dart:async';
 
 import 'package:chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
+  static final Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _currentUser = null;
+    _updateUser(_currentUser);
+  });
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
+
   @override
-  ChatUser? get currentUser => null;
+  ChatUser? get currentUser => _currentUser;
 
   @override
   Future<void> login(
     String email,
     String password,
-  ) {
-    throw UnimplementedError();
+  ) async {
+    _updateUser(_users[email]);
   }
 
   @override
-  Future<void> logout() {
-    throw UnimplementedError();
+  Future<void> logout() async {
+    _updateUser(null);
   }
 
   @override
@@ -26,10 +43,19 @@ class AuthMockService implements AuthService {
     String email,
     String password,
     File image,
-  ) {
-    throw UnimplementedError();
+  ) async {
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      password: password,
+      imageURL: image.path,
+    );
+
+    _users.putIfAbsent(email, () => newUser);
+    _updateUser(newUser);
   }
 
   @override
-  Stream<ChatUser?> get userChanges => throw UnimplementedError();
+  Stream<ChatUser?> get userChanges => _userStream;
 }
